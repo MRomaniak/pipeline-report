70261b25b5c92e11d5b1386cb2a7ae332956030a
MRomaniak <Rom.Mykola@gmail.com>
2022-05-08 19:51:14 +0300

    uncomment unit tests and align coverage evaluation approach

--- src/test/java/tk/idealist/pipeline/service/SuperReducedStringServiceTest.java
+++ src/test/java/tk/idealist/pipeline/service/SuperReducedStringServiceTest.java
@@ -21,31 +21,31 @@ class SuperReducedStringServiceTest {
         Assertions.assertEquals(EMPTY_STRING_OUTPUT, result);
     }
 
-//    @Test
-//    void testReduceWhenPassedSingleSymbol() {
-//        var input = "a";
-//        var result = superReducedStringService.reduce(input);
-//        Assertions.assertEquals(input, result);
-//    }
-//
-//    @Test
-//    void testReduceWhenTheSameSymbolsPassedTwice() {
-//        var input = "aa";
-//        var result = superReducedStringService.reduce(input);
-//        Assertions.assertEquals(EMPTY_STRING_OUTPUT, result);
-//    }
-//
-//    @Test
-//    void testReduceWhenEvenNumberOfTimesPassedTheSameSymbol() {
-//        var input = "bbbb";
-//        var result = superReducedStringService.reduce(input);
-//        Assertions.assertEquals(EMPTY_STRING_OUTPUT, result);
-//    }
-//
-//    @Test
-//    void testReduceWhenPassedSymboldAreReduced() {
-//        var input = "abbbC1223";
-//        var result = superReducedStringService.reduce(input);
-//        Assertions.assertEquals("abC13", result);
-//    }
+    @Test
+    void testReduceWhenPassedSingleSymbol() {
+        var input = "a";
+        var result = superReducedStringService.reduce(input);
+        Assertions.assertEquals(input, result);
+    }
+
+    @Test
+    void testReduceWhenTheSameSymbolsPassedTwice() {
+        var input = "aa";
+        var result = superReducedStringService.reduce(input);
+        Assertions.assertEquals(EMPTY_STRING_OUTPUT, result);
+    }
+
+    @Test
+    void testReduceWhenEvenNumberOfTimesPassedTheSameSymbol() {
+        var input = "bbbb";
+        var result = superReducedStringService.reduce(input);
+        Assertions.assertEquals(EMPTY_STRING_OUTPUT, result);
+    }
+
+    @Test
+    void testReduceWhenPassedSymboldAreReduced() {
+        var input = "abbbC1223";
+        var result = superReducedStringService.reduce(input);
+        Assertions.assertEquals("abC13", result);
+    }
 }
